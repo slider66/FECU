bd9604c77030605369da608f59a1c1f92386aedc
gokm8 <gouzb22@student.sdu.dk>
2025-04-12 15:04:45 +0200

    big commit

--- wedding-app/next.config.mjs
+++ wedding-app/next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.public.blob.vercel-storage.com",
+      },
+    ],
   },
 }
 
