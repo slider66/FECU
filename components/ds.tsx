4d4d56883ecb02bfdcf2bfd0193d02991520e334
gokm8 <gouzb22@student.sdu.dk>
2025-09-28 19:56:43 +0200

    change padding

--- components/ds.tsx
+++ components/ds.tsx
@@ -58,7 +58,7 @@ export const Section = ({ children, className, id, style }: DSProps) => (
  */
 export const Container = ({ children, className, id, style }: DSProps) => (
     <div
-        className={cn("max-w-5xl mx-auto p-4 sm:p-6", className)}
+        className={cn("max-w-xl mx-auto p-4 sm:p-6", className)}
         id={id}
         style={style}>
         {children}
