2484530e5d21e98c6efdd585a8758bbc8e0a067b
gokm8 <gouzb22@student.sdu.dk>
2025-09-28 20:23:21 +0200

    footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Layout } from "@/components/ds";
+import Footer from "@/components/wrapper/Footer";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
         <Layout>
             <body className={`${geistSans.variable} ${geistMono.variable}`}>
                 {children}
+                <Footer />
             </body>
         </Layout>
     );
---
+++ components/wrapper/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-center">
+            <p>Lavet med ♥️ af Gøkmen Øzbayir</p>
+        </footer>
+    );
+}
