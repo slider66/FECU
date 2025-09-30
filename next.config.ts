c2a83971901e97cbb1d8ce14827fa5cb261b3259
gokm8 <gouzb22@student.sdu.dk>
2025-10-01 00:32:03 +0200

    approval of remotePatterns to supabase images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            new URL(
+                "https://hqauvgxnshukhxjvzikv.supabase.co/storage/v1/object/public/wedding-photos/*"
+            ),
+        ],
+    },
 };
 
 export default nextConfig;
